7c636811134669d4c436d9dd563000740878419a
Jean Altidor <jeanaltidor@Jeans-MacBook-Pro.local>
2018-06-04 09:07:55 -0400

    moved

---
+++ server/src/main/java/java/ja/server/model/Customer.java
@@ -0,0 +1,13 @@
+package java.ja.server.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+
+@Entity
+@Table(name = "customer")
+public class Customer implements Serializable {
+
+
+
+}
